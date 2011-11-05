051fde2a6b6f744b1a656d4d59ab0924673b164e
Nicolas Vahlas <nico@vahlas.eu>
2011-11-05 18:42:01 +0200

    calculation api

---
+++ web/app/calculator.py
@@ -0,0 +1,16 @@
+from app.models import Participant
+
+class Calculator(object):
+    
+    def __init__(self, event):
+        self.event = event
+
+    def amount(self):
+        return 0;
+        
+    def participantAmount(self, participant):
+        
+        return 0;
+    
+    def participantBalance(self, participant):
+        return None;
